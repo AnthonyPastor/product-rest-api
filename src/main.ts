dbdc2d6bac746b27190a671537357847d68b17a9
Anthony <nahuel1pastor@gmail.com>
2023-03-31 20:13:45 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+
+	const config = new DocumentBuilder()
+		.setTitle('Product management API')
+		.setDescription(
+			'API to manage products, searches, payments, users and admin actions',
+		)
+		.setVersion('1.0')
+		.addTag('product-management')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
 	await app.listen(3001);
 }
 bootstrap();
